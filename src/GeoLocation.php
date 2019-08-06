84df9b53fddd50f14f5feba2018f3cf7d7d76149
Rafael Armenio <rafael.armenio@gmail.com>
2019-08-05 22:56:16 -0300

    Update GeoLocation.php

--- src/GeoLocation.php
+++ src/GeoLocation.php
@@ -4,7 +4,7 @@
  *
  * @link http://github.com/armenio for the source repository
  */
- 
+
 namespace Armenio\GeoLocation;
 
 use Zend\Http\Client;
@@ -12,56 +12,80 @@ use Zend\Http\Client\Adapter\Curl;
 use Zend\Json;
 
 /**
- * GeoLocation
+ * Class GeoLocation
+ * @package Armenio\GeoLocation
  */
 class GeoLocation
 {
-	public static function getLatLng($address)
-	{
-		$latLng = [];
+    /**
+     * @param string $address
+     * @param string $key
+     * @return array
+     */
+    public static function getLatLng($address, $key)
+    {
+        $url = 'http://maps.google.com/maps/api/geocode/json';
+        $client = new Client($url);
+        $client->setAdapter(new Curl());
+        $client->setMethod('GET');
+        $client->setOptions([
+            'curloptions' => [
+                CURLOPT_HEADER => false,
+                CURLOPT_CONNECTTIMEOUT => 0,
+                CURLOPT_TIMEOUT => 60,
+            ],
+        ]);
 
-		try{
-			$url = sprintf('http://maps.google.com/maps/api/geocode/json?address=%s&sensor=false', $address);
-			$client = new Client($url);
-			$client->setAdapter(new Curl());
-			$client->setMethod('GET');
-			$client->setOptions([
-				'curloptions' => [
-					CURLOPT_HEADER => false,
-				],
-			]);
+        $client->setParameterGet([
+            'address' => $address,
+            'sensor' => false,
+            'key' => $key,
+        ]);
 
-			
-			$response = $client->send();
-			
-			$body = $response->getBody();
-			
-			$result = Json\Json::decode($body, 1);
+        try {
+            $response = $client->send();
 
-			$latLng = [
-				'lat' => $result['results'][0]['geometry']['location']['lat'],
-				'lng' => $result['results'][0]['geometry']['location']['lng'],
-			];
+            $body = $response->getBody();
 
-			$isException = false;
-		} catch (\Zend\Http\Exception\RuntimeException $e){
-			$isException = true;
-		} catch (\Zend\Http\Client\Adapter\Exception\RuntimeException $e){
-			$isException = true;
-		} catch (Json\Exception\RuntimeException $e) {
-			$isException = true;
-		} catch (Json\Exception\RecursionException $e2) {
-			$isException = true;
-		} catch (Json\Exception\InvalidArgumentException $e3) {
-			$isException = true;
-		} catch (Json\Exception\BadMethodCallException $e4) {
-			$isException = true;
-		}
+            $arrResults = Json\Json::decode($body, 1);
 
-		if( $isException === true ){
-			//código em caso de problemas no decode
-		}
+            if (!empty($arrResults)) {
+                $result = [
+                    'lat' => $arrResults['results'][0]['geometry']['location']['lat'],
+                    'lng' => $arrResults['results'][0]['geometry']['location']['lng'],
+                ];
+            } else {
+                $result = [
+                    'error' => 'Dados não encontrados.',
+                ];
+            }
 
-		return $latLng;
-	}
+        } catch (Client\Adapter\Exception\TimeoutException $e) {
+            $result = [
+                'error' => $e->getMessage(),
+            ];
+        } catch (Client\Adapter\Exception\RuntimeException $e) {
+            $result = [
+                'error' => $e->getMessage(),
+            ];
+        } catch (Json\Exception\RecursionException $e) {
+            $result = [
+                'error' => $e->getMessage(),
+            ];
+        } catch (Json\Exception\RuntimeException $e) {
+            $result = [
+                'error' => $e->getMessage(),
+            ];
+        } catch (Json\Exception\InvalidArgumentException $e) {
+            $result = [
+                'error' => $e->getMessage(),
+            ];
+        } catch (Json\Exception\BadMethodCallException $e) {
+            $result = [
+                'error' => $e->getMessage(),
+            ];
+        }
+
+        return $result;
+    }
 }
